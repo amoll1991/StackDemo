9ecdaf6dca795e8a939b11d3ce076230bc7b5e8a
unknown <khapareamol@gmail.com>
2020-09-04 23:25:40 +0530

    Initial Commit

--- StackDemo/StackDemo.cs
+++ StackDemo/StackDemo.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace StackDemo
+namespace StackModel
 { 
-    class Program
+    class StackDemo
     {
         static void Main(string[] args)
         {
